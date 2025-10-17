c42d424d16ec217ea3cada505e1263a051ae3df7
flowz0 <billylflowers@gmail.com>
2025-10-16 22:07:11 -0700

    refactor(layout): move overflow-y-auto from About to parent div for proper layout control

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="max-w-7xl mx-auto min-h-screen px-6 pt-16 lg:pt-24 lg:grid lg:grid-cols-[400px_1fr]">
       <Sidebar />
-      <div className="lg:pl-48">
+      <div className="overflow-y-auto lg:pl-48">
         <About />
       </div>
     </main>
