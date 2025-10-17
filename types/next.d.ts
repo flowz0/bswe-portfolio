c5aec34ec2e1c88924afe421978089e9cb0aaca5
flowz0 <billylflowers@gmail.com>
2025-10-17 00:42:26 -0700

    feat: create ExperienceCardProps

--- types/next.d.ts
+++ types/next.d.ts
@@ -2,3 +2,12 @@ export interface ButtonProps {
   children: React.ReactNode;
   href: string;
 }
+
+export interface ExperienceCardProps {
+  dateRange: string;
+  title: string;
+  company: string;
+  summary: string;
+  link?: string;
+  tags: string[];
+}
