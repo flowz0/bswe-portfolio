546fe3869bea8859b5391447d34028e3e9f8890d
flowz0 <billylflowers@gmail.com>
2025-10-17 00:41:34 -0700

    feat: create responsive Experience component

---
+++ components/sections/Experience.tsx
@@ -0,0 +1,25 @@
+import ExperienceCard from "../ui/ExperienceCard";
+
+export default function Experience() {
+  return (
+    <section id="experience" className="mt-32 lg:scroll-mt-24">
+      <h2 className="text-h3 font-bold text-neutral-100 lg:hidden">Experience</h2>
+      <div className="flex flex-col gap-y-8 mt-4 lg:mt-0">
+        <ExperienceCard
+          dateRange="2025 - PRESENT"
+          title="Lead Engineer"
+          company="Netflows"
+          summary="Built and deployed a high-performance agency website using Next.js, TypeScript, and Tailwind CSS, achieving a 98 Web Core Vitals score through advanced optimization and best practices. I developed a custom multi-step booking form with real-time validation and accessible UX, boosting completion rates by 35%, and integrated SendGrid Email API with serverless automation to cut manual lead handling by 60% while managing deployment and analytics through Vercel and GA4."
+          tags={["TypeScript", "Next.js", "Tailwind CSS", "SendGrid Email API", "Vercel"]}
+        />
+        <ExperienceCard
+          dateRange="2025 - PRESENT"
+          title="Front End Engineer"
+          company="Bay Valley Tech"
+          summary="Contributed to the development of a full-stack social media platform, implementing core features such as a responsive navbar, dark/light mode toggle, and live user search using React and React Router. I optimized navigation with dynamic routing for user profiles, settings, and feed pages, improving usability and UI consistency. Working in an Agile Scrum environment, I collaborated with front-end and back-end teams through daily standups, code reviews, and sprint planning to deliver features on time and maintain a high-quality user experience."
+          tags={["JavaScript", "React", "React Router", "Bulma CSS", "Axios"]}
+        />
+      </div>
+    </section>
+  );
+}
