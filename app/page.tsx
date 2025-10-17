8755660e11328c92f9e058949c97c72355b4a76e
flowz0 <billylflowers@gmail.com>
2025-10-16 22:01:21 -0700

    feat: implement Sidebar component and main Page content into a responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Header from "@/components/layout/header";
+import About from "@/components/sections/About";
+import Sidebar from "@/components/sections/Sidebar";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
+    <main className="max-w-7xl mx-auto min-h-screen px-6 pt-16 lg:pt-24 lg:grid lg:grid-cols-[400px_1fr]">
+      <Sidebar />
+      <div className="lg:pl-48">
+        <About />
+      </div>
     </main>
   );
 }
