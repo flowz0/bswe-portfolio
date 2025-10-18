603bcc3c97e41f12eb7c3d74c611eca9ab780ddc
flowz0 <billylflowers@gmail.com>
2025-10-18 02:14:23 -0700

    feat: implement Footer component into app

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@ import About from "@/components/sections/About";
 import Sidebar from "@/components/layout/Sidebar";
 import Experience from "@/components/sections/Experience";
 import Projects from "@/components/sections/Projects";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto px-6 pt-16 lg:pt-24 lg:grid lg:grid-cols-[400px_1fr]">
+    <main className="max-w-7xl mx-auto px-6 py-16 lg:py-24 lg:grid lg:grid-cols-[400px_1fr]">
       <Sidebar />
       <div className="lg:pl-32">
         <About />
         <Experience />
         <Projects />
+        <Footer />
       </div>
     </main>
   );
