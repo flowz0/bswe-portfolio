c9ac3e2f645540ee20c251c0e9d1700d792dcfac
flowz0 <billylflowers@gmail.com>
2025-10-17 18:38:17 -0700

    feat: implement Projects componet into app and remove overflow-y-auto from parent div

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import About from "@/components/sections/About";
 import Sidebar from "@/components/layout/Sidebar";
 import Experience from "@/components/sections/Experience";
+import Projects from "@/components/sections/Projects";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto px-6 pt-16 lg:pt-24 lg:grid lg:grid-cols-[400px_1fr]">
       <Sidebar />
-      <div className="lg:pl-32 overflow-y-auto">
+      <div className="lg:pl-32">
         <About />
         <Experience />
+        <Projects />
       </div>
     </main>
   );
