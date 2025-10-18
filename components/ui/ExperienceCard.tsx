e632536422c9b37776c77cac42401a62069f99af
flowz0 <billylflowers@gmail.com>
2025-10-17 18:38:00 -0700

    fix: optimize spacing for consistency and improve UX

--- components/ui/ExperienceCard.tsx
+++ components/ui/ExperienceCard.tsx
@@ -12,12 +12,12 @@ export default function ExperienceCard({
   tags
 }: ExperienceCardProps) {
   return (
-    <div className="lg:grid lg:grid-cols-[160px_1fr]">
+    <div className="lg:grid lg:grid-cols-[160px_1fr] lg:gap-x-5">
       <div>
         <p className="text-sm font-bold text-neutral-500 cursor-default">{dateRange}</p>
       </div>
       <div>
-        <Link href={link || "/"} className="flex items-center gap-x-2 group w-fit">
+        <Link href={link || "/"} className="flex items-center gap-x-2 group w-fit mt-1">
           <h3 className="text-p font-bold text-neutral-100 transition-colors duration-300 ease-in-out group-hover:text-primary">
             {title} • {company}
           </h3>
