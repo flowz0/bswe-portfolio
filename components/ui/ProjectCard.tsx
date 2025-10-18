88db3fbefda81f7fb4b1a400aa31f3958986434e
flowz0 <billylflowers@gmail.com>
2025-10-17 18:33:18 -0700

    feat: create reusable ProjectCard component

---
+++ components/ui/ProjectCard.tsx
@@ -0,0 +1,40 @@
+// import Image from "next/image";
+import Link from "next/link";
+import { FaArrowRight, FaImage } from "react-icons/fa";
+import Tag from "./Tag";
+import { ProjectCardProps } from "@/types/next";
+
+export default function ProjectCard({
+  title,
+  summary,
+  link,
+  tags
+}: ProjectCardProps) {
+  return (
+    <div className="flex flex-col lg:grid lg:grid-cols-[160px_1fr] lg:gap-x-5">
+      {/* <div className="order-2"> */}
+      <div className="order-2 mt-4 lg:mt-0 lg:order-1 w-full sm:w-64 lg:w-full h-48 lg:h-28 flex items-center justify-center rounded-lg bg-neutral-800">
+        <FaImage className="size-6 text-neutral-700" />
+      </div>
+      {/* </div> */}
+      <div className="mt-4 order-1 lg:mt-0">
+        <Link href={link || "/"} className="flex items-center gap-x-2 group w-fit">
+          <h3 className="text-p font-bold text-neutral-100 transition-colors duration-300 ease-in-out group-hover:text-primary">
+            {title}
+          </h3>
+          <div>
+            <FaArrowRight className="size-6 -rotate-45 transition-colors duration-300 ease-in-out group-hover:text-primary" />
+          </div>
+        </Link>
+        <p className="text-p text-neutral-300 mt-2">
+          {summary}
+        </p>
+        <div className="flex flex-wrap gap-2 mt-4">
+          {tags?.map((tag, index) => (
+            <Tag key={index}>{tag}</Tag>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
