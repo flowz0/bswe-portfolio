57d052ce4a9158350c131bb08c49ba392d73f6c1
flowz0 <billylflowers@gmail.com>
2025-10-18 15:27:13 -0700

    fix: add bottom padding for mobile only in Footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-32">
+    <footer className="mt-32 pb-16 lg:pb-0">
       <div>
         <p className="text-p text-neutral-300 max-w-md">
           Designed in <Link href="https://www.figma.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Figma</Link> and developed in <Link href="https://code.visualstudio.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Visual Studio Code</Link>. Built with <Link href="https://nextjs.org/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Next.js</Link> and <Link href="https://tailwindcss.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Tailwind CSS</Link>, deployed with <Link href="https://vercel.com/home" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Vercel</Link>.
