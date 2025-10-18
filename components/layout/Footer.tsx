2264724f6437551f341013d5225ab123486ef4c7
flowz0 <billylflowers@gmail.com>
2025-10-18 15:04:03 -0700

    fix: add hover text color and target attribute set to "_blank" in Footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mt-32">
       <div>
         <p className="text-p text-neutral-300 max-w-md">
-          Designed in <Link href="https://www.figma.com/" className="text-neutral-100 font-bold">Figma</Link> and developed in <Link href="https://code.visualstudio.com/"  className="text-neutral-100 font-bold">Visual Studio Code</Link>. Built with <Link href="https://nextjs.org/"  className="text-neutral-100 font-bold">Next.js</Link> and <Link href="https://tailwindcss.com/"  className="text-neutral-100 font-bold">Tailwind CSS</Link>, deployed with <Link href="https://vercel.com/"  className="text-neutral-100 font-bold">Vercel</Link>.
+          Designed in <Link href="https://www.figma.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Figma</Link> and developed in <Link href="https://code.visualstudio.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Visual Studio Code</Link>. Built with <Link href="https://nextjs.org/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Next.js</Link> and <Link href="https://tailwindcss.com/" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Tailwind CSS</Link>, deployed with <Link href="https://vercel.com/home" target="_blank" className="text-neutral-100 font-bold transition-colors duration-300 ease-in-out hover:text-primary">Vercel</Link>.
         </p>
       </div>
     </footer>
