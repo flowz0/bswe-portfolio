c1f1e8a7434212ee8c5ff51d50c782da72813861
flowz0 <billylflowers@gmail.com>
2025-10-17 17:04:16 -0700

    fix: set text small and add default cursor for Tag component

--- components/ui/Tag.tsx
+++ components/ui/Tag.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Tag({children}: {children: ReactNode}) {
   return (
-    <div className="py-1 px-3 w-fit rounded-full bg-neutral-700 text-neutral-300">
+    <div className="py-1 px-3 w-fit rounded-full text-sm cursor-default bg-neutral-700 text-neutral-300">
       <p>{children}</p>
     </div>
   );
