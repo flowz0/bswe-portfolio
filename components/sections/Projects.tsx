9fad13e57847a9cffaf006921dc433ccb0374bab
flowz0 <billylflowers@gmail.com>
2025-10-18 15:45:06 -0700

    fix: update card Link and remove project placeholder in Projects component

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -12,13 +12,7 @@ export default function Projects() {
         <ProjectCard
           title="Real-Time Chat Application"
           summary="Developed a real-time chat application enabling instant private messaging with Socket.IO, achieving low latency and smooth responsiveness across sessions. The project featured JWT authentication, bcryptjs encryption, and cookie-based sessions to ensure secure account creation and persistent logins. Designed a scalable back-end architecture using Node.js and MongoDB capable of handling multiple concurrent connections while maintaining reliable message delivery and data integrity."
-          link="/"
-          tags={["Express", "Node.js", "Socket.IO", "MongoDB", "JWT", "bcryptjs"]}
-        />
-        <ProjectCard
-          title="Real-Time Chat Application"
-          summary="Developed a real-time chat application enabling instant private messaging with Socket.IO, achieving low latency and smooth responsiveness across sessions. The project featured JWT authentication, bcryptjs encryption, and cookie-based sessions to ensure secure account creation and persistent logins. Designed a scalable back-end architecture using Node.js and MongoDB capable of handling multiple concurrent connections while maintaining reliable message delivery and data integrity."
-          link="/"
+          link="https://github.com/flowz0/chat-app"
           tags={["Express", "Node.js", "Socket.IO", "MongoDB", "JWT", "bcryptjs"]}
         />
       </div>
