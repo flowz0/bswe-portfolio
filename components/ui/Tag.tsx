bb3726a815cbb9ea74d17a298ef81e8c48ab15e7
flowz0 <billylflowers@gmail.com>
2025-10-27 23:52:00 -0700

    fix: update Tag component custom styling

--- components/ui/Tag.tsx
+++ components/ui/Tag.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Tag({children}: {children: ReactNode}) {
   return (
-    <div className="py-1 px-3 w-fit rounded-full text-sm cursor-default bg-neutral-700 text-neutral-300">
+    <div className="text-neutral-300 bg-neutral-900 text-sm py-1 px-3 rounded-full w-fit cursor-default">
       <p>{children}</p>
     </div>
   );
